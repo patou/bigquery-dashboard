fef3246885e0c150fc6f2a4aef56ee8e1288d145
Angélo LIMA <alima@externe.generali.fr>
2014-10-27 14:13:56 +0100

    ajout du callback repetitif interrogeant le job

--- bigquery-dashbord-war/src/main/webapp/js/controllers/homeController.js
+++ bigquery-dashbord-war/src/main/webapp/js/controllers/homeController.js
@@ -4,9 +4,10 @@ app.controller('HomeController', function($scope, $location, AuthService, $http,
     AuthService.refresh();
     $scope.user = AuthService.getUser();
     $scope.items = {};
+    $scope.resuls = {};
 
     $http.get("/api/service/queries")
-        .success(function (data, status, headers, config) {
+        .success(function (data) {
             $scope.items = eval(data);
             ngProgress.complete();
         })
@@ -18,17 +19,17 @@ app.controller('HomeController', function($scope, $location, AuthService, $http,
         return "/login?path=" + $location.path();
     };
 
-    $scope.$watch(function () { return AuthService.getUser(); }, function (newVal, oldVal) {
+    $scope.$watch(function () { return AuthService.getUser(); }, function () {
         $scope.user = AuthService.getUser();
     });
 
     $scope.isAuthenticated = AuthService.isAuthenticated();
-    $scope.$watch(function () { return AuthService.isAuthenticated(); }, function (newVal, oldVal) {
+    $scope.$watch(function () { return AuthService.isAuthenticated(); }, function () {
         $scope.isAuthenticated = AuthService.isAuthenticated();
     });
 
     $scope.isAdmin = AuthService.isAdmin();
-    $scope.$watch(function () { return AuthService.isAuthenticated(); }, function (newVal, oldVal) {
+    $scope.$watch(function () { return AuthService.isAuthenticated(); }, function () {
         $scope.isAdmin = AuthService.isAdmin();
     });
 
@@ -38,12 +39,40 @@ app.controller('HomeController', function($scope, $location, AuthService, $http,
         ngProgress.reset();
         ngProgress.start();
         $http.post("/api/execute/query/",item)
-            .success(function (data, status, headers, config){
+            .success(function (data){
                 console.log(data);
                 ngProgress.complete();
+                requestCallback(data.jobId);
             })
             .error(function(error){
                 console.log(error);
             });
+    };
+
+    var requestCallback = function (jobId) {
+        var stop;
+        $scope.fight = function () {
+            if (angular.isDefined(stop)) return;
+
+            stop = $interval(function () {
+                $http.get("/api/execute/result/"+jobId)
+                    .success(function (data) {
+                        if(data.jobComplete === true){
+                            $scope.stopFight();
+                            $scope.resuls = eval(data);
+
+                        }
+                    })
+                    .error(function (error) {
+
+                    });
+            }, 1000);
+        };
+        $scope.stopFight = function() {
+            if (angular.isDefined(stop)) {
+                $interval.cancel(stop);
+                stop = undefined;
+            }
+        };
     };
 });
