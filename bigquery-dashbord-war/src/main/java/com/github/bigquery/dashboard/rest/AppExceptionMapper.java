eaa995eb23f1a5df5308b2b3a54316e6aef88cca
Patrice de Saint Steban <patou.de.saint.steban@gmail.com>
2014-12-17 17:12:23 +0100

    catch errors in bigquery service

---
+++ bigquery-dashbord-war/src/main/java/com/github/bigquery/dashboard/rest/AppExceptionMapper.java
@@ -0,0 +1,20 @@
+package com.github.bigquery.dashboard.rest;
+
+import com.google.api.client.googleapis.json.GoogleJsonError;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
+
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.ext.ExceptionMapper;
+
+public class AppExceptionMapper implements ExceptionMapper<GoogleJsonResponseException> {
+ 
+    public Response toResponse(GoogleJsonResponseException ex) {
+        GoogleJsonError details = ex.getDetails();
+        return Response.status(details.getCode())
+                .entity(details.getMessage())
+                .type(MediaType.APPLICATION_JSON).
+                build();
+    }
+ 
+}
--- bigquery-dashbord-war/src/main/java/com/github/bigquery/dashboard/service/ExecuteBigQueryService.java
+++ bigquery-dashbord-war/src/main/java/com/github/bigquery/dashboard/service/ExecuteBigQueryService.java
@@ -2,6 +2,7 @@ package com.github.bigquery.dashboard.service;
 
 import com.github.bigquery.dashboard.util.Config;
 import com.github.bigquery.dashboard.util.CredentialsUtils;
+import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.bigquery.Bigquery;
 import com.google.api.services.bigquery.model.GetQueryResultsResponse;
 import com.google.api.services.bigquery.model.JobReference;
@@ -10,19 +11,28 @@ import com.google.api.services.bigquery.model.QueryResponse;
 
 import java.io.IOException;
 import java.security.GeneralSecurityException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  * 21/10/2014.
  */
 public class ExecuteBigQueryService {
+    static Logger logger = Logger.getLogger(ExecuteBigQueryService.class.getName());
     public static JobReference startQuery(String query) throws GeneralSecurityException, IOException {
         Bigquery bigquery = CredentialsUtils.getBigquery();
         QueryRequest request = new QueryRequest();
         request.setQuery(query);
         request.setTimeoutMs(0L);
         request.setUseQueryCache(true);
-        QueryResponse result = bigquery.jobs().query(Config.BIGQUERY_PROJECTID, request).execute();
-        return result.getJobReference();
+        try {
+            QueryResponse result = bigquery.jobs().query(Config.BIGQUERY_PROJECTID, request).execute();
+            return result.getJobReference();
+        }
+        catch (GoogleJsonResponseException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw e;
+        }
     }
 
     public static String tryQuery(String query) throws GeneralSecurityException, IOException {
@@ -30,13 +40,25 @@ public class ExecuteBigQueryService {
         QueryRequest request = new QueryRequest();
         request.setQuery(query);
         request.setDryRun(true);
-        QueryResponse result = bigquery.jobs().query(Config.BIGQUERY_PROJECTID, request).execute();
-        return "OK";
+        try {
+            QueryResponse result = bigquery.jobs().query(Config.BIGQUERY_PROJECTID, request).execute();
+            return "OK";
+        }
+        catch (GoogleJsonResponseException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw e;
+        }
     }
 
     public static GetQueryResultsResponse getResult(String jobId) throws GeneralSecurityException, IOException {
         Bigquery bigquery = CredentialsUtils.getBigquery();
-        GetQueryResultsResponse result = bigquery.jobs().getQueryResults(Config.BIGQUERY_PROJECTID, jobId).execute();
-        return result;
+        try {
+            GetQueryResultsResponse result = bigquery.jobs().getQueryResults(Config.BIGQUERY_PROJECTID, jobId).execute();
+            return result;
+        }
+        catch (GoogleJsonResponseException e) {
+            logger.log(Level.SEVERE, e.getMessage(), e);
+            throw e;
+        }
     }
 }
