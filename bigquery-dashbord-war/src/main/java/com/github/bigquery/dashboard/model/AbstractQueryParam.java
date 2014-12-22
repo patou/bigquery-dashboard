2b556174978f1d42ea1f297b4cf4daae12c3e149
Patrice de Saint Steban <patou.de.saint.steban@gmail.com>
2014-12-22 11:15:55 +0100

    jersey json polymorphism

--- bigquery-dashbord-war/src/main/java/com/github/bigquery/dashboard/model/AbstractQueryParam.java
+++ bigquery-dashbord-war/src/main/java/com/github/bigquery/dashboard/model/AbstractQueryParam.java
@@ -6,6 +6,9 @@ import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
 import com.googlecode.objectify.annotation.Parent;
 import com.googlecode.objectify.annotation.Subclass;
+import org.codehaus.jackson.annotate.JsonSubTypes;
+import org.codehaus.jackson.annotate.JsonTypeInfo;
+import org.codehaus.jackson.annotate.JsonTypeName;
 
 import java.util.Date;
 import java.util.List;
@@ -14,16 +17,23 @@ import java.util.List;
  * Created by b002kla on 18/12/2014.
  */
 @Entity
+@JsonTypeInfo(use=JsonTypeInfo.Id.NAME, include=JsonTypeInfo.As.PROPERTY, property="type")
+@JsonSubTypes({
+        @JsonSubTypes.Type(value=AbstractQueryParam.TextQueryParam.class, name="TEXT"),
+        @JsonSubTypes.Type(value=AbstractQueryParam.DateQueryParam.class, name="DATE"),
+        @JsonSubTypes.Type(value=AbstractQueryParam.ListQueryParam.class, name="LIST"),
+        @JsonSubTypes.Type(value=AbstractQueryParam.NumberQueryParam.class, name="NUMBER"),
+})
 public abstract class AbstractQueryParam {
     @Parent
     private Key<BigQuery> bigQuery;
     @Id
     @Expose
-    private String name;
+    protected String name;
     @Expose
-    private String label;
+    protected String label;
     @Expose
-    private Type type;
+    protected Type type;
 
     public Key<BigQuery> getBigQuery() {
         return bigQuery;
@@ -62,9 +72,14 @@ public abstract class AbstractQueryParam {
     }
 
     @Subclass
+    @JsonTypeName("TEXT")
     public static class TextQueryParam extends AbstractQueryParam {
         @Expose
         private String defaultValue;
+
+        public TextQueryParam() {
+            type = Type.TEXT;
+        }
 
         public String getDefaultValue() {
             return defaultValue;
@@ -75,9 +90,14 @@ public abstract class AbstractQueryParam {
         }
     }
     @Subclass
+    @JsonTypeName("DATE")
     public static class DateQueryParam extends AbstractQueryParam {
         @Expose
         private Date defaultValue;
+
+        public DateQueryParam() {
+            type = Type.DATE;
+        }
 
         public Date getDefaultValue() {
             return defaultValue;
@@ -88,9 +108,14 @@ public abstract class AbstractQueryParam {
         }
     }
     @Subclass
+    @JsonTypeName("NUMBER")
     public static class NumberQueryParam extends AbstractQueryParam {
         @Expose
         private Number defaultValue;
+
+        public NumberQueryParam() {
+            type = Type.NUMBER;
+        }
 
         public Number getDefaultValue() {
             return defaultValue;
@@ -101,11 +126,16 @@ public abstract class AbstractQueryParam {
         }
     }
     @Subclass
+    @JsonTypeName("LIST")
     public static class ListQueryParam extends AbstractQueryParam {
         @Expose
         private List<String> values; // for Type LIST
         @Expose
         private String defaultValue;
+
+        public ListQueryParam() {
+            type = Type.LIST;
+        }
 
         public List<String> getValues() {
             return values;
